2c0505515cebd56ac17ed60bbdabd641170f6c5b
Flavio Wuensche <fwuensch@student.42.us.org>
2018-12-12 20:07:35 +0100

    Finish again

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwuensch <fwuensch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/29 15:50:33 by fwuensch          #+#    #+#             */
+/*   Updated: 2018/12/12 20:01:17 by fwuensch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+# include "libft/libft.h"
+
+# define BUFF_SIZE 32
+
+int		get_next_line(const int fd, char **line);
+
+#endif
